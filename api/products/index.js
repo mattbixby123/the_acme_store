81dc0d00f15d2d0df6c077dc17fcb16dea81be65
Matthew Bixby <matthewbixby@matthews-air.myfiosgateway.com>
2024-03-23 14:41:21 -0400

    last route for GET /api/products added

--- api/products/index.js
+++ api/products/index.js
@@ -1,3 +1,15 @@
 const router = require('express').Router();
+
+const { fetchProducts } = require('../../db');
+
 // GET /api/products - returns an array of products
+router.get('/', async (req, res, next) => {
+  try {
+    const products = await fetchProducts();
+    res.status(200).send(products);
+  } catch (error) {
+    next(error)
+  }
+});
+
 module.exports = router;
